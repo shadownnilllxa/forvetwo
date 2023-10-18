e589cb94fdffd0727ef9d5a545712eda7830169a
ZxScripter <131220168+ZxScripter@users.noreply.github.com>
2023-10-18 16:25:52 +0530

    config.py

--- config.py
+++ config.py
@@ -11,19 +11,19 @@ from logging.handlers import RotatingFileHandler
 load_dotenv("config.env")
 
 # Bot token dari @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "609fUKUqpMxwTGT5Iz48n68")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "KT5Iz48n68")
 
 # API ID Anda dari my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "25996"))
 
 # API Hash Anda dari my.telegram.org
-API_HASH = os.environ.get("API_HASH", "1f5343b59fc248f")
+API_HASH = os.environ.get("API_HASH", "3c248f")
 
 # ID Channel Database
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-10867474"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-108474"))
 
 # NAMA OWNER
-OWNER = os.environ.get("OWNER", "Sensei_Rimuru")
+OWNER = os.environ.get("OWNER", "Suru")
 
 # Protect Content
 PROTECT_CONTENT = strtobool(os.environ.get("PROTECT_CONTENT", "False"))
@@ -36,11 +36,11 @@ HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
 UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 
 # Database
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://userbot:userbot@cluster0.ltasu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv:/myFes=true&w=majority"
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001980485"))
-FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-100169041")) 
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-100485"))
+FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-109041")) 
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
